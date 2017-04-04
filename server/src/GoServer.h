c868b3ae358b8b82cedc538f5c6cc756559c7859
Cloogo <wang691234@gmail.com>
2017-04-04 23:10:58 +0800

    add server/src

---
+++ server/src/GoServer.h
@@ -0,0 +1,34 @@
+#ifndef _GOSERVER_H
+#define _GOSERVER_H
+
+#include <muduo/net/TcpServer.h>
+#include <boost/noncopyable.hpp>
+#include "PacketParser.h"
+
+
+class GoServer:boost::noncopyable{
+public:
+    GoServer(muduo::net::EventLoop* loop,
+             const muduo::net::InetAddress& listenAddr);
+
+    ~GoServer();
+
+    void start();
+
+    void onConnection(const muduo::net::TcpConnectionPtr& conn);
+
+    void receive(const muduo::net::TcpConnectionPtr& conn,
+                   muduo::net::Buffer* buf,
+                   muduo::Timestamp time);
+    
+     void send(const muduo::net::TcpConnectionPtr& conn,
+            std::string& message);
+   
+
+private:
+
+    muduo::net::TcpServer server_;
+    const static size_t kHeaderLen = sizeof(int32_t);
+};
+
+#endif
---
+++ server/src/PacketParser.cc
@@ -0,0 +1,16 @@
+#include "PacketParser.h"
+#include <redbud/parser/json_parser.h>
+
+using namespace redbud::parser::json;
+
+void
+PacketParser::encode(){
+}
+
+void
+PacketParser::decode(){
+}
+
+void
+PacketParser::dispatch(){
+}
