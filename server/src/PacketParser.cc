fb0a0ad4a76bcfb6fa1647feae311c2b102846c8
Cloogo <wang691234@gmail.com>
2017-04-13 21:23:10 +0800

    reconstruct the channel transferring msg

--- server/src/PacketParser.cc
+++ server/src/PacketParser.cc
@@ -1,16 +1,56 @@
 #include "PacketParser.h"
+#include "Proto.h"
 #include <redbud/parser/json_parser.h>
-
 using namespace redbud::parser::json;
+using namespace std;
 
 void
 PacketParser::encode(){
+    auto str=out.dumps();
+    msgCb(conn,str);
 }
 
 void
 PacketParser::decode(){
+    in=Json::parse(msg);
 }
 
 void
 PacketParser::dispatch(){
+    decode();
+    switch(REQUEST_TYPE(in["request type"])){
+        case REQUEST_TYPE::LOGIN:
+            Login login(in);
+            out=login.handle();
+            break;
+        case REQUEST_TYPE::REGIST:
+            Regist regist(in);
+            out=regist.handle();
+        break;
+        case REQUEST_TYPE::LOGOUT:
+        break;
+        case REQUEST_TYPE::FETCH_PLAYER_INFO:
+        break;
+        case REQUEST_TYPE::FETCH_LOBBY_INFO:
+        break;
+        case REQUEST_TYPE::FETCH_ROOM_INFO:
+        break;
+        case REQUEST_TYPE::SIT_DOWN:
+        break;
+        case REQUEST_TYPE::LEAVE:
+        break;
+        case REQUEST_TYPE::READGO:
+        break;
+        case REQUEST_TYPE::GIVEUP:
+        break;
+        case REQUEST_TYPE::PLACECHESS:
+        break;
+        case REQUEST_TYPE::GAMEOVER_WINNER:
+        break;
+        case REQUEST_TYPE::GAMEOVER_LOSER:
+        break;
+        case REQUEST_TYPE::SEND_MSG:
+        break;
+    }
+    encode();
 }
--- server/src/PacketParser.h
+++ server/src/PacketParser.h
@@ -9,8 +9,9 @@ class PacketParser:boost::noncopyable{
 public:
     typedef boost::function<void (const muduo::net::TcpConnectionPtr&,
                                   std::string&)> msgCb;
-    explicit PacketParser(const std::string msg,
-                          const msgCb& msgCb):msg_(msg),msgCb_(msgCb){
+    explicit PacketParser(const TcpConnectionPtr& conn_,
+                          const std::string msg_,
+                          const msgCb& msgCb_):conn(conn_),msg(msg_),msgCb(msgCb_){
     }
 
     ~PacketParser(){}
@@ -21,8 +22,10 @@ public:
 
     void decode();
 private:
-    msgCb msgCb_;
-    std::string msg_;
-    Entrance entrance_;
+    msgCb msgCb;
+    const muduo::net::TcpConnectionPtr conn;
+    std::string msg;
+    redbud::parser::json::Json in;
+    redbud::parser::json::Json out;
 };
 #endif
