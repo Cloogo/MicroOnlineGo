63eaa4a482c27884e95fc57c59d158989599f67e
Cloogo <wang691234@gmail.com>
2017-04-18 13:59:19 +0800

    add mysql table

---
+++ server/sql/init.sql
@@ -0,0 +1,22 @@
+drop database go;
+create database go character set utf8mb4 collate utf8mb4_unicode_ci;
+use go;
+drop table if exists users;
+create table users(
+account varchar(10),
+nickname varchar(10),
+passwd varchar(10),
+level int,
+integral int,
+status varchar(10),
+primary key(account)
+);
+drop table if exists rooms;
+create table rooms(
+id int,
+name varchar(10),
+player1 varchar(10),
+player2 varchar(10),
+status varchar(10),
+primary key(id)
+);
