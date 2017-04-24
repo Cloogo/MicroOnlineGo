dd0f42653780186e58fcc12312f50501a5aaf248
Cloogo <wang691234@gmail.com>
2017-04-24 20:09:07 +0800

    add update game result

---
+++ server/src/GameResult.cc
@@ -0,0 +1,24 @@
+#include "GameResult.h"
+#include "Proto.h"
+#include "SqlStm.h"
+
+#define T RESPONSE_TYPE
+
+using namespace redbud::parser::json;
+
+GameResult::GameResult(Json in){
+    account=in["account"].as_string();
+    level=in["level"].as_number();
+    integer=in["integer"].as_number();
+}
+
+Json
+GameResult::handle(){
+    std::string stm0="update users set level="+to_string(level)+",integer="+to_string(integer)+" where account=\""+account+"\"";
+    if(SqlStm::silence(stm0)){
+        out["response_type"]=int(T::UPDATE_GAMERESULT_SUCCESS);
+    }else{
+        out["response_type"]=int(T::UPDATE_GAMERESULT_FAILED);
+    }
+    return out;
+}
---
+++ server/src/GameResult.h
@@ -0,0 +1,17 @@
+#ifndef _GAMERESULT_H
+#define _GAMERESULT_H
+
+#include <redbud/parser/json_parser.h>
+
+class GameResult{
+public:
+    GameResult(redbud::parser::json::Json in);
+    ~GameResult(){}
+    redbud::parser::json::Json handle();
+private:
+    redbud::parser::json::Json out;
+    std::string account;
+    int level;
+    int integer;
+};
+#endif
