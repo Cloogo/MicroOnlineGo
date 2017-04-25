b0d5cc9b002ca4a11725bdec38a21c8480d5c62f
Cloogo <wang691234@gmail.com>
2017-04-25 19:08:30 +0800

    fix variable type

--- server/src/Handshake.cc
+++ server/src/Handshake.cc
@@ -3,13 +3,15 @@
 #include "SqlManager.h"
 #include "SqlStm.h"
 #include "RoomManager.h"
+#include <muduo/base/Logging.h>
+
 
 #define T RESPONSE_TYPE
 using namespace redbud::parser::json;
 using namespace std;
 
 Handshake::Handshake(Json in){
-    id=int(in["id"].as_number());
+    id=in["id"].as_number();
     nickname=in["nickname"].as_string();
     order=ORDER(int(in["order"].as_number()));
 }
@@ -22,6 +24,7 @@ Handshake::handle(){
     }else if(order==ORDER::RIGHT){
         stm0="update rooms set readygo2=\"yes\" where id="+to_string(id);
     }
+    LOG_INFO<<"Handshake: stm0: "<<stm0;
     if(SqlStm::silence(stm0)){
         string stm1="select * from rooms where id="+to_string(id);
         string stm2="select * from rooms where id="+to_string(id);
