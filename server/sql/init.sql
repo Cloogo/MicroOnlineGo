2685f8313e572758af1ea5299a342b46e1bf25c9
Cloogo <wang691234@gmail.com>
2017-05-16 13:23:56 +0800

    reconstruct table

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -1,5 +1,5 @@
 drop database go;
-create database go character set utf8mb4 collate utf8mb4_unicode_ci;
+create database go character set utf8 collate utf8_general_ci;
 use go;
 drop table if exists users;
 create table users(
@@ -7,8 +7,16 @@ account varchar(10),
 nickname varchar(10),
 passwd varchar(10),
 level int,
-integral int,
-status varchar(10),
+rank int,
+win int,
+lose int,
+draw int,
+year int,
+month tinyint,
+day tinyint,
+sex tinyint,
+state tinyint,
+roomid int,
 primary key(account)
 );
 drop table if exists rooms;
@@ -17,8 +25,12 @@ id int,
 name varchar(10),
 player1 varchar(10),
 player2 varchar(10),
-readygo1 varchar(3),
-readygo2 varchar(3),
-status varchar(10),
+state varchar(10),
+komi tinyint,
+mainTime tinyint,
+period tinyint,
+periodTimes tinyint,
+player1ready tinyint,
+player2ready tinyint,
 primary key(id)
 );
